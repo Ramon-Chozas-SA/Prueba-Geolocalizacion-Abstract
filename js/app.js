43d9eba9aed0d0754dc75ec93240a01d91de39b5
unknown <ortiztato@gmail.com>
2023-01-03 16:16:50 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+const buscarLocation = async () => {
+
+    console.log('buscando location');
+
+    const response = await fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=dd5ba78a1a014a009d6625959e125259');
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'No encontro location');
+    }
+
+    console.log('data' + data)
+    console.log({ data })
+    console.log('data.body' + data.body)
+
+};
